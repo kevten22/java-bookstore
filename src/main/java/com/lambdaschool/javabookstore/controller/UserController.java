e18f516701310b0d8207f05b2243295f21c9b7e1
Kevin Tena <kevten09@gmail.com>
2019-03-01 12:55:14 -0600

    Added Authentication config files, assigned roles, created user model, repo, and controller

---
+++ src/main/java/com/lambdaschool/javabookstore/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.lambdaschool.javabookstore.controller;
+
+import com.lambdaschool.javabookstore.model.User;
+import com.lambdaschool.javabookstore.repository.Userrepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+
+    @Autowired
+    // private UserService userService;
+    private Userrepository userrepos;
+
+    @GetMapping("/users")
+    public List<User> listAllUsers()
+    {
+        return userrepos.findAll();
+    }
+
+    @PostMapping("/user")
+    public User addNewUser(@RequestBody User newuser) throws URISyntaxException
+    {
+        return userrepos.save(newuser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUserById(@PathVariable long id)
+    {
+        var foundUser = userrepos.findById(id);
+        if (foundUser.isPresent())
+        {
+            userrepos.deleteById(id);
+
+            return "{" + "\"id\":"   + foundUser.get().getId() +
+                    ",\"usename\":" + "\"" + foundUser.get().getUsername() + "\"" +
+                    ",\"role\":" + foundUser.get().getAuthority() + "}";
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+}
