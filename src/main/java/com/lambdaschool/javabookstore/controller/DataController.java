5d9667de4fbfb4163d8769dbcdc2d0671e2533c6
Kevin Tena <kevten09@gmail.com>
2019-03-01 13:07:23 -0600

    Added custom Swagger documentation

--- src/main/java/com/lambdaschool/javabookstore/controller/DataController.java
+++ src/main/java/com/lambdaschool/javabookstore/controller/DataController.java
@@ -4,14 +4,16 @@ import com.lambdaschool.javabookstore.model.Book;
 import com.lambdaschool.javabookstore.repository.Authorrepository;
 import com.lambdaschool.javabookstore.repository.Bookrepository;
 import com.lambdaschool.javabookstore.repository.Sectionrepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.net.URISyntaxException;
 import java.util.Optional;
 
+@Api(value = "Todo Application", description = "Lambda School JX: Sprint Challenge 3")
 @RestController
 @RequestMapping(value = "/data/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DataController {
@@ -25,6 +27,7 @@ public class DataController {
     @Autowired
     Sectionrepository sectionrepos;
 
+    @ApiOperation(value = "Edits a book based on its ID, requires a request body matching the schema", response = Book.class)
     @PutMapping("/books/{id}")
     public Book changeBookById(@RequestBody Book newbook, @PathVariable long bookid) throws URISyntaxException{
         Optional<Book> updateBook = bookrepos.findById(bookid);
@@ -44,12 +47,13 @@ public class DataController {
             return null;
     }
 
-    //@PathVariable("zooid", long zooid), @PathVariable("animalid") long animalid
+    @ApiOperation(value = "Connect a book from the request body to the authorid in our joined wrote table. Request body must be a book object with a bookid", response = Book.class)
     @PostMapping("/books/authors/authorid}")
     public String assignBooktoAuthor(@RequestBody Book book, @PathVariable long authorid){
         return bookrepos.addBooktoAuthor(book.getBookid(),authorid);
     }
 
+    @ApiOperation(value = "Deletes a book based on the ID provided", response = Book.class)
     @DeleteMapping("/books/{id}")
     public String deleteBookById(@PathVariable long bookid){
         var foundBook = bookrepos.findById(bookid);
